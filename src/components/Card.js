b83f83e23ae63e0fdf80222143deac51559a7745
Travis Johnston <tjohnst1@gmail.com>
2018-02-04 11:29:15 -0800

    add Card component; rename Forecast to Weather; install classnames

---
+++ src/components/Card.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Weather from './Weather';
+import classNames from 'classnames';
+import '../styles/card.scss'
+
+export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        lat: null,
+        lng: null,
+        zip: null,
+        city: null,
+        state: null,
+        country: null,
+      },
+      temp: {
+        current: null,
+        feelsLike: null,
+        summary: null,
+        icon: null,
+      },
+      upcoming: [],
+    }
+    this.geolocateUser = this.geolocateUser.bind(this);
+    this.getCityInfo = this.getCityInfo.bind(this);
+    this.getWeatherInfo = this.getWeatherInfo.bind(this);
+  }
+
+  geolocateUser() {
+    return fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.REACT_APP_GOOGLE_KEY}`, { method: 'POST' })
+      .then(response => response.json())
+      .then(json => ({
+        lat: json.location.lat,
+        lng: json.location.lng,
+      }))
+  }
+
+  getCityInfo(coords) {
+    const { lat, lng } = coords;
+    return fetch(`/api/city/${lat}/${lng}`)
+      .then(res => res.json())
+      .then(json => {
+        return ({
+          lat: lat,
+          lng: lng,
+          zip: json.zip,
+          city: json.city,
+          state: json.state,
+          country: json.country,
+        })
+      })
+  }
+
+  getWeatherInfo(cityInfo) {
+    const { lat, lng } = cityInfo;
+    return fetch(`/api/weather/${lat}/${lng}`)
+      .then(res => res.json())
+      .then(json => ({
+        location: cityInfo,
+        temp: json.temp,
+        upcoming: json.upcoming,
+      }))
+  }
+
+  componentDidMount() {
+    this.geolocateUser()
+      .then(coords => this.getCityInfo(coords))
+      .then(cityInfo => this.getWeatherInfo(cityInfo))
+      .then(completeInfo => {
+        this.setState({
+          location: completeInfo.location,
+          temp: completeInfo.temp,
+          upcoming: completeInfo.upcoming,
+        })
+      });
+  }
+
+  render() {
+    const { location, temp, upcoming } = this.state;
+    console.log(this.props)
+    const { flipped } = this.props;
+    const props = { location, temp, upcoming, flipped };
+    const classes = classNames({
+      card: true,
+      flipped,
+    })
+    return (
+      <div className={classes}>
+        <Weather {...props} />
+      </div>
+    )
+  }
+}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import UpcomingForecast from './UpcomingForecast';
-import '../styles/forecast.scss'
+import classNames from 'classnames';
+import '../styles/weather.scss'
 
 export default class Forecast extends Component {
   static propTypes = {
@@ -26,26 +27,31 @@ export default class Forecast extends Component {
         day: PropTypes.string,
       })
     ).isRequired,
+    hide: false,
   };
 
   render() {
-    const { location, temp, upcoming } = this.props;
+    const { location, temp, upcoming, flipped } = this.props;
+    const classes = classNames({
+      weather: true,
+      flipped,
+    });
     return (
-      <div className="forecast">
-        <div className="forecast__header">
-          <p className="forecast__date">Today</p>
-          <p className="forecast__location">{location.city}</p>
+      <div className={classes}>
+        <div className="weather__header">
+          <p className="weather__date">Today</p>
+          <p className="weather__location">{location.city}</p>
         </div>
-        <div className="forecast__img">
+        <div className="weather__img">
           <svg style={{margin: "0 auto"}} width="150" height="150" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1472 896q0-117-45.5-223.5t-123-184-184-123-223.5-45.5-223.5 45.5-184 123-123 184-45.5 223.5 45.5 223.5 123 184 184 123 223.5 45.5 223.5-45.5 184-123 123-184 45.5-223.5zm276 277q-4 15-20 20l-292 96v306q0 16-13 26-15 10-29 4l-292-94-180 248q-10 13-26 13t-26-13l-180-248-292 94q-14 6-29-4-13-10-13-26v-306l-292-96q-16-5-20-20-5-17 4-29l180-248-180-248q-9-13-4-29 4-15 20-20l292-96v-306q0-16 13-26 15-10 29-4l292 94 180-248q9-12 26-12t26 12l180 248 292-94q14-6 29 4 13 10 13 26v306l292 96q16 5 20 20 5 16-4 29l-180 248 180 248q9 12 4 29z"/></svg>
           <p>{temp.description}</p>
         </div>
-        <div className="forecast__temp">
+        <div className="weather__temp">
           <p>{Math.round(temp.current)}&deg;</p>
-          <p className="forecast__feels-like">Feels like {Math.round(temp.feelsLike)}&deg;</p>
+          <p className="weather__feels-like">Feels like {Math.round(temp.feelsLike)}&deg;</p>
           <p>{temp.summary}</p>
         </div>
-        <div className="forecast__upcoming">
+        <div className="weather__upcoming">
           {upcoming.length > 0 ? upcoming.map((data, i) => <UpcomingForecast icon={data.icon} temp={data.temp} day={data.day} key={i} />) : null}
         </div>
       </div>
